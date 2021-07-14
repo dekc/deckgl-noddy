8df27937e81ecc3a846d5906a93f66612c69b5a9
Don Carpenter <doca@dhigroup.com>
2021-07-14 20:24:50 +1000

    Improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,86 @@
-import React, { useEffect, useState } from 'react'
-import  { FlyToInterpolator } from 'react-map-gl';
-import { InteractiveMapProps } from 'react-map-gl/src/components/interactive-map';
-import { ViewState } from 'react-map-gl/src/mapbox/mapbox';
-import { csv } from 'd3';
+import React, { useEffect, useState } from "react";
+import { FlyToInterpolator } from "react-map-gl";
+import { InteractiveMapProps } from "react-map-gl/src/components/interactive-map";
+import { ViewState } from "react-map-gl/src/mapbox/mapbox";
+import { csv, json } from "d3";
 
-import  Map from './Map';
-import './App.css'
+import Map from "./Map";
+import "./App.css";
 // import { ViewState } from 'react-map-gl/src/mapbox/mapbox';
-import { capitals } from './locations';
+import { capitals } from "./locations";
 
-const TrafficInter = 'https://data.gov.au/dataset/f28878de-4ea1-4ae2-a342-a08c2a5a4e7a/resource/9e58f0b8-9250-4cd6-9937-017c3510f654/download/rrcsignalisedintersections.csv';
-const wsb = 'https://data.gov.au/data/dataset/1b853228-5bc4-42e6-82e7-5d43a563251d/resource/323ae3a5-66b8-432c-8b64-ddda32fc93e6/download/wasteboundary.kmz';
+const TrafficInter =
+  "https://data.gov.au/dataset/f28878de-4ea1-4ae2-a342-a08c2a5a4e7a/resource/9e58f0b8-9250-4cd6-9937-017c3510f654/download/rrcsignalisedintersections.csv";
+const wsb =
+  "https://data.gov.au/data/dataset/1b853228-5bc4-42e6-82e7-5d43a563251d/resource/323ae3a5-66b8-432c-8b64-ddda32fc93e6/download/wasteboundary.kmz";
+const Boundry = "http://localhost:8989/boundryllproj.json";
+const Mesh = "http://localhost:8989/mesh_original.geo.json";
 
 function App() {
-const [viewState, setViewState] = useState<ViewState>(capitals.Goldie)
-const [data, setData] = useState([]);
-const [boundaries, setBoundaries] = useState([]);
+  const [viewState, setViewState] = useState<ViewState>(capitals.Goldie);
+  const [data, setData] = useState([]);
+  const [boundries, setBoundries] = useState();
+  const [mesh, setMesh] = useState();
 
-useEffect(() => {
-  csv(TrafficInter, (d: any, ind: number) => ({
-    id: ind,
-    fid: d.FID,
-    position: [+d.Longitude, +d.Latitude] 
+  useEffect(() => {
+    csv(TrafficInter, (d: any, ind: number) => ({
+      id: ind,
+      fid: d.FID,
+      position: [+d.Longitude, +d.Latitude],
+    })).then((tid: any) => {
+      setData(tid);
+    });
 
-  }))
-  .then((tid:any) => {
-    setData(tid)
-  });
+    json(Boundry).then((data: any) => {
+      setBoundries(data);
+    });
 
+    json(Mesh).then((data: any) => {
+      setMesh(data);
+    });
+  }, []);
 
+  const handleViewStateChange = ({
+    viewState,
+  }: {
+    viewState: InteractiveMapProps;
+  }) => setViewState(viewState);
 
-}, [])
-
-const handleViewStateChange = ({viewState}: {viewState: InteractiveMapProps}) => setViewState(viewState)
-
-const handleFlyTo = (destination: InteractiveMapProps) => {
-  setViewState({
-    ...viewState, 
-    ...destination,
-  })
-}
-
+  const handleFlyTo = (destination: InteractiveMapProps) => {
+    setViewState({
+      ...viewState,
+      ...destination,
+    });
+  };
 
   return (
     <div className="App box">
-      <header id="header">
-        Map demo
-      </header>
+      <header id="header">DeckGL Sandbox</header>
       <main id="content">
-        
-        <Map 
-          width="100vw" 
-          height="100%" 
+        <Map
+          width="100vw"
+          height="100%"
           viewState={viewState}
           onViewStateChange={handleViewStateChange}
           transitionInterpolator={new FlyToInterpolator()}
           transitionDuration={2000}
-          data={data}
+          svgdata={data}
+          boundrydata={boundries}
+          meshdata={mesh}
         />
         <div className="controls">
-          {Object.keys(capitals).map(key => {
-            return <button key={key} onClick={() => handleFlyTo(capitals[key])}>{key}</button>
+          {Object.keys(capitals).map((key) => {
+            return (
+              <button key={key} onClick={() => handleFlyTo(capitals[key])}>
+                {key}
+              </button>
+            );
           })}
         </div>
       </main>
-      <footer id="footer">
-        Map Footer
-      </footer>
+      <footer id="footer">Map Footer</footer>
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Map.tsx
+++ src/Map.tsx
@@ -1,23 +1,70 @@
-import React from 'react'
-import MapGL, {SVGOverlay} from 'react-map-gl';
-import { InteractiveMapProps } from 'react-map-gl/src/components/interactive-map';
+import React from "react";
+import MapGL, { Layer, LayerProps, Source, SVGOverlay } from "react-map-gl";
+import { InteractiveMapProps } from "react-map-gl/src/components/interactive-map";
+
+const boundryStyle: LayerProps = {
+  type: "line",
+  id: "boundry",
+  paint: {
+    "line-color": "#32361e",
+    "line-width": 2,
+    "line-opacity": 0.8,
+  },
+};
+
+const meshStyle: LayerProps = {
+  type: "fill",
+  id: "mesh",
+  paint: {
+    "fill-color": [
+      "rgb",
+      ["*", -1, ["/", 200, ["get", "Bathymetry"]]],
+      0,
+      ["+", 255, ["/", 255, ["get", "Bathymetry"]]],
+    ],
+    "fill-opacity": 0.75,
+    "fill-outline-color": "#970101",
+  },
+};
 
 export type MapProps = InteractiveMapProps & {
   width: number | string;
   height: number | string;
-  data: any;
+  svgdata: any;
+  boundrydata: any;
+  meshdata: any;
 };
 
-const Map = ({width, height, data, ...rest }: MapProps) => {
+const Map = ({
+  width,
+  height,
+  svgdata,
+  boundrydata,
+  meshdata,
+  ...rest
+}: MapProps) => {
   return (
-    <MapGL width={width} height={height} 
+    <MapGL
+      width={width}
+      height={height}
       mapboxApiAccessToken={import.meta.env.VITE_MapboxAccessToken as string}
+      interactiveLayerIds={["boundrydata"]}
       {...rest}
-      >
-        <SvgOverlayData data={data} radius={12} />
+    >
+      <SvgOverlayData data={svgdata} radius={12} />
+      <GeoJsonOverlayData
+        data={meshdata}
+        layerId="boundry"
+        layerStyle={meshStyle}
+      />
+      <GeoJsonOverlayData
+        data={boundrydata}
+        layerId="mesh"
+        layerStyle={boundryStyle}
+      />
     </MapGL>
-  )
-}
+  );
+};
 
 type SvgOverlayDataProps = {
   data: any;
@@ -25,17 +72,37 @@ type SvgOverlayDataProps = {
 };
 
 const SvgOverlayData = ({ data, radius }: SvgOverlayDataProps) => {
-  const redraw = ({ project }: {project: Function}) => {
+  const redraw = ({ project }: { project: Function }) => {
     return (
       <g>
         {data.map((datum: any) => {
-          const [x,y] = project(datum.position);
-          return <circle key={datum.fid} cx={x} cy={y} r={radius} fill="tomato" />
+          const [x, y] = project(datum.position);
+          return (
+            <circle key={datum.fid} cx={x} cy={y} r={radius} fill="tomato" />
+          );
         })}
       </g>
-    )
+    );
   };
-  return <SVGOverlay redraw={redraw} />
-}
+  return <SVGOverlay redraw={redraw} />;
+};
 
-export default Map
+type GeoJsonOverlayDataProps = {
+  data: any;
+  layerId: string;
+  layerStyle: LayerProps;
+};
+
+const GeoJsonOverlayData = ({
+  data,
+  layerId,
+  layerStyle,
+}: GeoJsonOverlayDataProps) => {
+  return (
+    <Source id={layerId} type="geojson" data={data} tolerance={0.2}>
+      <Layer {...layerStyle} />
+    </Source>
+  );
+};
+
+export default Map;
